4509540b9c926135143714a90d838acfe7af19a7
Praeploy Kiatsuksri <praeploy05@gmail.com>
2025-04-03 11:12:57 +0700

    add a Swagger comment (Hospital model) in routes/hospitals.js

--- routes/hospitals.js
+++ routes/hospitals.js
@@ -31,6 +31,62 @@ const express = require('express');
 const {protect,authorize} = require('../middleware/auth');
 const {getHospitals,getHospital,createHospital,updateHospital,deleteHospital} = require('../controller/hospitals');
 
+//hospital schema YAML
+/**
+ * @swagger
+ * components:
+ *      schemas:
+ *          Hospital:
+ *              type: object
+ *              required:
+ *                  - name
+ *                  - address
+ *              properties:
+ *                  id:
+ *                      type: string
+ *                      format: uuis
+ *                      description: The auto-generated id of the hospital
+ *                      example: d290f1ee-6c54-4b01-90e6-d701748f0851
+ *                  ลำดับ:
+ *                      type: string
+ *                      description: Ordinal number
+ *                  name:
+ *                      type: string
+ *                      description: Hospital name
+ *                  address:
+ *                      type: string
+ *                      description: House No., Street, Road
+ *                  district:
+ *                      type: string
+ *                      description: District
+ *                  province:
+ *                      type: string
+ *                      description: province
+ *                  postalcode:
+ *                      type: string
+ *                      description: 5-digit postal code
+ *                  tel:
+ *                      type: string
+ *                      description: telephone number
+ *                  region:
+ *                      type: string
+ *                      description: region
+ *              example:
+ *                  id: 609bda561452242d88d36e37
+ *                  ลำดับ: 121
+ *                  name: Happy Hospital
+ *                  address: 121 ถ.สุขุมวิท
+ *                  district: บางนา
+ *                  province: กรุงเทพมหานคร
+ *                  postalcode: 10110
+ *                  tel: 02-2187000
+ *                  region: กรุงเทพมหานคร(Bangkok)
+ *                      
+ * 
+ */
+
+
+
 
 //include other resource routers
 const appointmentRouter = require('./appointments');
