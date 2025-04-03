80824b4157f40fbd90a024664cdfebaca45d17d7
Praeploy Kiatsuksri <praeploy05@gmail.com>
2025-04-03 11:24:18 +0700

    add server url in server.js

--- server.js
+++ server.js
@@ -44,11 +44,11 @@ const swaggerOptions = {
             version:'1.0.0',
             description:'A simple Express VacQ API'
         },
-        /*servers: [
+        servers: [
             {
                 url: 'http://localhost:5000/api/v1'
             }
-        ]*/
+        ]
     },
     apis:['./routes/*.js'],
 };
